4489697e22094640c63ff5afedf4246624fe1e88
AdsonBruno <adsonbruno2@gmail.com>
2022-08-12 12:53:52 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:bonfire/bonfire.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Game Island',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BonfireTiledWidget(
+      joystick: Joystick(
+        directional: JoystickDirectional(
+          color: Colors.black54,
+        ),
+      ),
+      map: TiledWorldMap(
+        'map/island.json',
+        forceTileSize: const Size(32, 32),
+      ),
+    );
+  }
+}
