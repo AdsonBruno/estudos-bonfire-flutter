78f7b3091ceba9af5ecb67dce6c6009a230b4e22
AdsonBruno <adsonbruno2@gmail.com>
2022-08-13 13:37:33 -0300

    refatoracao do nome da modulo e classe. game_sprite_sheet -> player_sprite_sheet

---
+++ lib/player_sprite_sheet.dart
@@ -0,0 +1,41 @@
+import 'package:bonfire/bonfire.dart';
+
+class PlayerSpriteSheet {
+  static Future<SpriteAnimation> get heroIdLeft => SpriteAnimation.load(
+        'fHero_.png',
+        SpriteAnimationData.sequenced(
+          amount: 4,
+          stepTime: 0.15,
+          textureSize: Vector2(24, 24),
+          texturePosition: Vector2(96, 0),
+        ),
+      );
+
+  static Future<SpriteAnimation> get heroIdRight => SpriteAnimation.load(
+        'fHero_.png',
+        SpriteAnimationData.sequenced(
+          amount: 4,
+          stepTime: 0.15,
+          textureSize: Vector2(24, 24),
+          texturePosition: Vector2(0, 0),
+        ),
+      );
+  static Future<SpriteAnimation> get heroRunRight => SpriteAnimation.load(
+        'fHero_.png',
+        SpriteAnimationData.sequenced(
+          amount: 4,
+          stepTime: 0.15,
+          textureSize: Vector2(24, 24),
+          texturePosition: Vector2(0, 48),
+        ),
+      );
+  static Future<SpriteAnimation> get heroRunLeft => SpriteAnimation.load(
+        'fHero_.png',
+        SpriteAnimationData.sequenced(
+          amount: 4,
+          stepTime: 0.15,
+          textureSize: Vector2(24, 24),
+          texturePosition: Vector2(96, 48),
+        ),
+      );
+}
