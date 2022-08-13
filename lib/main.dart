ef57d2140f655b23f55b8195c62dc1ef2cba88ad
AdsonBruno <adsonbruno2@gmail.com>
2022-08-13 13:17:37 -0300

    refatoracao do tamanho, retirando trecho que força o tamanho e adicionando perspectiva de camera e fixando o apenas o trecho visivel do mapa

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:bonfire/bonfire.dart';
 import 'package:flutter/material.dart';
 import 'package:game_island/hero.dart';
 
-const double tileSize = 32;
+const double tileSize = 16;
 void main() {
   runApp(const MyApp());
 }
@@ -36,9 +36,18 @@ class MyHomePage extends StatelessWidget {
       ),
       map: TiledWorldMap(
         'map/island.tmj',
-        forceTileSize: const Size(tileSize, tileSize),
       ),
-      player: GameHero(Vector2(18 * tileSize, 14 * tileSize)),
+      player: GameHero(
+        Vector2(18 * tileSize, 14 * tileSize),
+      ),
+      cameraConfig: CameraConfig(
+        moveOnlyMapArea: true,
+        zoom: 2.5,
+        sizeMovementWindow: Vector2(
+          tileSize * 3,
+          tileSize * 3,
+        ),
+      ),
     );
   }
 }
